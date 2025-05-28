63aa665e97cb48cb0d7e5f74af322bde3faea6d9
Jb192101 <grigorijbaryshev910@gmail.com>
2025-05-28 19:31:41 +0300

    Добавление ограничения на кол-во создавемых окон

--- BookStatistic/src/main/java/org/jedi_bachelor/view/View.java
+++ BookStatistic/src/main/java/org/jedi_bachelor/view/View.java
@@ -2,5 +2,24 @@ package org.jedi_bachelor.view;
 
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 abstract public class View extends Stage {
+    // Поле для ограничения кол-ва окон (Singleton)
+    protected static Map<Class<?>, Integer> windowCountMap = new HashMap<>();
+
+    public View() {
+        Class<?> currentClass = this.getClass();
+        int currentCount = windowCountMap.getOrDefault(currentClass, 0);
+
+        if (currentCount >= 1) {
+            throw new IllegalStateException(
+                    "Превышен лимит окон для " + currentClass.getSimpleName() +
+                            " (максимум " + 1 + ")"
+            );
+        }
+
+        windowCountMap.put(currentClass, currentCount + 1);
+    }
 }
