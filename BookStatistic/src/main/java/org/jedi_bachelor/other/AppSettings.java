50fed21a5da6175b1ff78269c0a4c756256a0ec1
Jb192101 <grigorijbaryshev910@gmail.com>
2025-06-04 12:48:23 +0300

    Добавление класса настроек

---
+++ BookStatistic/src/main/java/org/jedi_bachelor/other/AppSettings.java
@@ -0,0 +1,12 @@
+package org.jedi_bachelor.other;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+public class AppSettings {
+    @Getter
+    @Setter
+    private String language;
+}
